002762d807e2f4373f1d8e344715d5d03c483bc9
GCH <glen.chevalier@sirehna.com>
2024-11-04 17:27:47 +0000

    :construction: Reworking launch file

--- src/self_balancing_robot/launch/robot_display.launch.py
+++ src/self_balancing_robot/launch/robot_display.launch.py
@@ -1,30 +1,77 @@
 import os
+import xacro
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+
 
 def generate_launch_description():
-    # Path to the Gazebo world file
-    world_file = os.path.join(get_package_share_directory('self_balancing_robot'), 'worlds', 'self_balancing_world.world')
+    # Launch arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    # Gazebo launch file to launch the world
-    gazebo_launch = ExecuteProcess(
-            cmd=['gz', 'sim', world_file],
-            output='screen'
-        )
-    
-    # Robot State Publisher to display the URDF in Gazebo
-    urdf_file = os.path.join(get_package_share_directory('self_balancing_robot'), 'urdf', 'self_balancing_robot.urdf')
-    robot_state_publisher = Node(
+    # Paths to important directories
+    robot_description_path = get_package_share_directory('self_balancing_robot')
+    gazebo_launch_path = os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+
+    # Process xacro file to generate URDF
+    xacro_file = os.path.join(robot_description_path, 'urdf', 'self_balancing_robot.urdf.xacro')
+    doc = xacro.process_file(xacro_file, mappings={'use_sim': 'true'})
+    robot_desc = doc.toprettyxml(indent='  ')
+    params = {'robot_description': robot_desc}
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gazebo_launch_path)
+    )
+
+    # Robot state publisher node
+    node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': open(urdf_file).read()}]
+        parameters=[params],
+        arguments=[use_sim_time]
+    )
+
+    # Spawn entity node
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-string', robot_desc,
+            '-x', '0.0',
+            '-y', '0.0',
+            '-z', '0.4',
+            '-R', '0.0',
+            '-P', '0.0',
+            '-Y', '0.0',
+            '-name', 'balancing_robot',
+            '-allow_renaming', 'false'
+        ]
     )
     
 
+    # Bridge node for connecting Gazebo and ROS 2
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        output='screen'
+    )
+
+    # Launch description
     return LaunchDescription([
-        gazebo_launch, 
-        robot_state_publisher
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        gazebo,
+        node_robot_state_publisher,
+        gz_spawn_entity,
+        bridge
     ])
